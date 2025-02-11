fcb3cd3c10e7d11da3b8460c52d7e7bc37b4ad82
李昭 <lizhao@zhechengdata.com>
2025-02-11 18:12:38 +0800

    feat: 初始化 autoHrm 项目
    - 创建 Spring Boot 应用程序主类和测试类
    - 添加基本控制器和路径变量控制器- 创建用户类和静态 HTML 文件
    - 设置 MyBatis 配置和数据库连接
    - 添加 Gradle 构建脚本和相关配置
    - 创建 .gitignore 文件
    
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/qingtangbaimian/autohrm/AutoHrmApplication.java
@@ -0,0 +1,13 @@
+package com.qingtangbaimian.autohrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoHrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoHrmApplication.class, args);
+	}
+
+}
